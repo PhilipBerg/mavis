4360e0f68394034837d90897584613b9bc6d67d0
PhilipBerg <pberg@live.se>
2024-01-22 09:51:36 -0600

    Updated partitioning

--- R/trend_partitioning.R
+++ R/trend_partitioning.R
@@ -18,7 +18,8 @@ utils::globalVariables(
 #' @export
 #'
 #' @examples # Please see vignette('baldur') for examples
-trend_partitioning <- function(data, design_matrix, formula = sd ~ mean * c, eps = .1, eps_scale = "log-linear", n = 1000, verbose = T){
+trend_partitioning <- function(data, design_matrix, formula = sd ~ mean * c, eps = .1, eps_scale = "log-linear", verbose = T, max_iter = 15)
+  {
   sd_col <- as.character(formula)[2]
   if (length(eps) == 2) {
     pseq <- purrr::partial(seq, length.out = sum(!is.na(data[[sd_col]])))
@@ -36,23 +37,41 @@ trend_partitioning <- function(data, design_matrix, formula = sd ~ mean * c, eps
     cur_data <- data
   }
   cur_data <- cur_data %>%
-    prep_data_for_clustering(formula, eps = eps, n = n) %>%
-    run_procedure(formula, eps = eps, n = n)
+    prep_data_for_clustering(formula, eps = eps) %>%
+    run_procedure(formula, eps = eps)
+  ll_val <- -Inf
+  counter <- 0
   while (cur_data$i > 1) {
     if (verbose) {
-      print(paste("Moved", cur_data$i, "points"))
+      cli::cli_alert_info("Moved {cur_data$i} points")
     }
+    counter <- counter + 1
     cur_data <- cur_data$data %>%
       run_procedure(
         formula,
-        eps = eps, n = n
+        eps = eps
       )
+    if (length(unique(cur_data$data$c)) == 1) {
+      cli::cli_alert_danger("Partition converged to a single cluster\nBreaking and returning single partition")
+      cli::cli_alert_warning("Try a different window size; ignore if part of windows size estimation")
+      break
+    }
+    ll_val_cur <- cur_data$data %>%
+      fit_gamma_regression(formula) %>%
+      estimate_gamma_hyperparameters(cur_data$data) %$%
+      ll(sd, alpha[1], beta)
+    if (ll_val_cur > ll_val) {
+      ll_val <- ll_val_cur
+    } else if (ll_val_cur < ll_val & counter >= max_iter) {
+      if (verbose) print("Global optima increased; breaking")
+      break
+    }
   }
   if (anyNA(data[[sd_col]])) {
     out <- cur_data$data %>%
       cluster_missing_sd(
         data, design_matrix,
-        formula, max(eps), n
+        formula, max(eps)
       )
   }
   else {
@@ -62,7 +81,7 @@ trend_partitioning <- function(data, design_matrix, formula = sd ~ mean * c, eps
     dplyr::select(-c(betal, betau, intl, intu, alpha))
 }
 
-prep_data_for_clustering <- function(data, formula, eps = .1, n = 1000){
+prep_data_for_clustering <- function(data, formula, eps = .1){
   fc <- as.character(formula)
   data_ms <- data %>%
     tidyr::drop_na(fc[2])
@@ -103,42 +122,51 @@ resetimate_gamma_pars <- function (data, formula, gm = NULL)
   )
 }
 
-add_integrals <-function (data, eps, n = 1000, sd_col)
+add_integrals <-function (data, eps, sd_col)
 {
   data %>%
     dplyr::mutate(
-      intu = num_int_trapz(!!sd_col, alpha, betau, eps, n),
-      intl = num_int_trapz(!!sd_col, alpha, betal, eps, n)
+      intu = num_int_trapz(!!sd_col, alpha, betau, eps),
+      intl = num_int_trapz(!!sd_col, alpha, betal, eps)
     )
 }
 
-run_procedure <- function (data, formula, eps, n = 1000)
+run_procedure <- function (data, formula, eps)
 {
   data %>%
     resetimate_gamma_pars(formula) %>%
     add_integrals(
       eps = eps,
-      n = n, rlang::sym(as.character(formula)[2])
+      rlang::sym(as.character(formula)[2])
     ) %>%
     clust_itt_norm()
 }
 
-num_int_trapz <- function (sd, alpha, beta, eps, n)
+num_int_trapz <- function (sd, alpha, beta, eps)
 {
   if (length(eps) == 1) {
     eps <- rep(eps, times = length(sd))
   }
+
   rng <- purrr::map2(sd, eps, ~ c(-.y, .y) + .x)
-  x <- purrr::map(rng, ~ seq(.x[1], .x[2], length.out = n))
-  dx <- purrr::map(x, diff)
-  y <- purrr::pmap(list(x, alpha, beta), stats::dgamma)
-  y <- purrr::map(y, ~ (.x[1:(length(.x)-1)] + .x[2:length(.x)]))
-  purrr::map2_dbl(y, dx,
-                  ~ sum(.x*.y)
-  )
+
+  purrr::pmap_dbl(list(rng, alpha, beta), ~ pgamma(..1[2], ..2, ..3) - pgamma(..1[1], ..2, ..3))
+
+
+  # x <- purrr::map(rng, ~ seq(.x[1], .x[2], length.out = n))
+  # x <- purrr::map(x, ~ .x[.x>=0])
+  # dx <- purrr::map(x, diff)
+  #
+  # y <- purrr::pmap(list(x, alpha, beta), stats::dgamma)
+  # y <- purrr::map(y, ~ (.x[1:(length(.x)-1)] + .x[2:length(.x)]))
+  #
+  # purrr::map2_dbl(y, dx,
+  #                 ~ sum(.x*.y)
+  # )
 }
 
-cluster_missing_sd <- function(clustered_data, data, design_matrix, formula, eps, n){
+cluster_missing_sd <- function(clustered_data, data, design_matrix, formula, eps)
+  {
   sigma_all <- data %>%
     dplyr::select(dplyr::matches(get_conditions(design_matrix))) %>%
     unlist() %>%
@@ -151,10 +179,18 @@ cluster_missing_sd <- function(clustered_data, data, design_matrix, formula, eps
       !!sd_col := sigma_all
     ) %>%
     resetimate_gamma_pars(formula, gam_mod) %>%
-    add_integrals(eps, n, sd_col = sd_col) %>%
+    add_integrals(eps, sd_col = sd_col) %>%
     dplyr::mutate(
       c = dplyr::if_else(intl < intu, 'U', 'L'),
       !!sd_col := NA
     ) %>%
     dplyr::bind_rows(clustered_data, .)
+}
+
+ll <- function(x, alpha, beta)
+  {
+  alpha*sum(log(beta)) -
+    length(x)*log(gamma(alpha)) +
+    (alpha - 1)*sum(log(x)) -
+    sum(beta*x)
 }
